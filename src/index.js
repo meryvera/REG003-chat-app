43354a591a34732d82cc910a197f5dde2d92e291
meryvera <yrem.vera.5@gmail.com>
2021-10-06 16:41:24 -0500

    Starting express & socket io config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
-console.log("Aló B");
-console.log("Aló Mark");
+/* console.log("Aló B");
+console.log("Aló Mark"); */
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http'); // cambia por const CoinGecko = require('coingecko-api');
+const server = http.createServer(app);
+
+app.use(cors())
+
+/* socket */
+//const { Server } = require("socket.io");
+//const io = new Server(server);
+//----------
+
+const io = require('socket.io')(server, {
+  cors:{
+    origin: ['*']
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+/* socket */
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  // the server gets it as a chat message event
+   socket.on('chat message', (msg) => {
+       console.log('message: ' + msg);
+       io.emit('chat message', msg);
+   });
+});
+//----------
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
