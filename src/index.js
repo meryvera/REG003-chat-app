2232f77caf5ad6172d264dea8b584cf836e64289
Yessenia Vela <vela.kathy@gmail.com>
2021-10-06 16:37:06 -0500

    crea rama colabYess

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Aló B")
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// This will emit the event to all connected sockets
+// io.emit('some event', { 
+//     someProperty: 'some value', 
+//     otherProperty: 'other value' 
+// }); 
+io.on('connection', (socket) => {
+     // the server gets it as a chat message event
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
