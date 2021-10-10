78cfbffa88f4ea88e27a4174bd76fc23e079d0e4
meryvera <yrem.vera.5@gmail.com>
2021-10-09 22:47:03 -0500

    Feat .env config ports BE-FE

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const config = require('../global/config');
+
 const app = express();
+
 const http = require('http'); 
 const server = http.createServer(app);
+
+const { portBE, portFE } = config;
 
 app.use(cors())
 
 const io = require('socket.io')(server, {
   cors:{
-    origin: ['http://localhost:4200'],
+    origin: [portFE],
     credentials: true,
     methods: ["GET", "POST"]
   }
-  // cors:{
-  //    origin:true,
-  //    credentials: true,
-  //    methods: ["GET", "POST"]
-  //}
 })
 
 app.get('/', (req, res) => {
@@ -28,12 +28,12 @@ io.on('connection', (socket) => {
   console.log('Nuevo usuario conectado');
   // the server gets it as a chat message event
    socket.on('chat message', (objet) => {
-       console.log('message: ' + objet.msg);
-       io.emit('chat message', objet);
-       // socket.emit ("testing Kathy")
+      console.log('message: ' + objet.msg);
+      io.emit('chat message', objet);
+      // socket.emit ("testing Kathy")
    });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(portBE, () => {
+  console.log(`App listening on port ${portBE} =D`);
 });
