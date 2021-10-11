2a0b66e4bb18b5388edc47e1d40c26d104dd4d01
meryvera <yrem.vera.5@gmail.com>
2021-10-11 10:55:12 -0500

    Fixing initial settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('../global/config');
+const pkg = require('../package.json')
 
 const app = express();
 
@@ -9,13 +10,20 @@ const server = http.createServer(app);
 
 const { portBE, portFE } = config;
 
+app.set('config', config);
+app.set('pkg', pkg);
+
+// parse application/x-www-form-urlencoded
 app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//app.use(authMiddleware(secret)); -ERROR
 
 const io = require('socket.io')(server, {
   cors:{
     origin: [portFE],
     credentials: true,
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"] //son los metodos que usa internamente socke.io
   }
 })
 
@@ -24,15 +32,23 @@ app.get('/', (req, res) => {
 });
 
 /* socket */
-io.on('connection', (socket) => {
-  console.log('Nuevo usuario conectado');
+io.on('connection', (client) => { //on escucha eventos connection, 1 vez que hay respuesta del cb (socket), manejo esas asincronioas
+  console.log('Nuevo usuario conectado' /* + client */);//[object Object]
   // the server gets it as a chat message event
-   socket.on('chat message', (objet) => {
-      console.log('message: ' + objet.msg);
-      io.emit('chat message', objet);
-      // socket.emit ("testing Kathy")
-   });
+  client.on('sendMessage', (messageInfo) => {
+    console.log('message: ' + messageInfo.text); //message: Hola Kathy Angular - recibido desde el FE
+    client.broadcast.emit('receiveMessage', messageInfo); // mandado del BE hacia el FE
+    // socket.emit ("testing Kathy")
+  });
+
+  client.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  })
 });
+
+// io.on('disconnect', () => {
+//   console.log('Usuario desconectado');
+// });
 
 server.listen(portBE, () => {
   console.log(`App listening on port ${portBE} =D`);
