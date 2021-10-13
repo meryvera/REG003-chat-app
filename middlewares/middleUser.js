da259548423824ef274582c5171fee84314d71e9
Yessenia Vela <vela.kathy@gmail.com>
2021-10-13 13:30:21 -0500

    create middleware USER for validation result

--- middlewares/middleUser.js
+++ middlewares/middleUser.js
@@ -1,1 +1,24 @@
-/* Socket io middleware */
+// const { isValidEmail } = require('../utils/utils');
+const { validationResult } = require('express-validator');
+
+const middlewareCreateUser = async (req, resp, next) => {
+
+    try {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log('21');
+        return resp.status(400).json({ errors: errors.array() });
+        }
+        return next()
+
+    } catch (error) {
+        console.log('25 middle:', error);
+        return next(error)
+    }
+
+};
+  
+  module.exports = {
+      middlewareCreateUser
+  }
