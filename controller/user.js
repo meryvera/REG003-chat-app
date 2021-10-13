b2bf2af44d4035f958d77471844ea91ed0646dd1
meryvera <yrem.vera.5@gmail.com>
2021-10-13 18:54:07 -0500

    Feat hasshing passwords new users

--- controller/user.js
+++ controller/user.js
@@ -4,26 +4,28 @@ const bcrypt = require('bcrypt');
 
 /* Creando User */
 const createUser = async (req, resp, next) => {
-    const { name, email, password } = req.body;
+
+  const { name, email, password } = req.body;
+  
+  try {
+
+    const salt = bcrypt.genSaltSync();
+    const hashedPassword = bcrypt.hashSync(password, salt);
     
-    try {
-        // const salt = bcrypt.genSaltSync();
-        // user.password = bcrypt.hashSync(password, salt);
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword
+      }
+    });
+    
+    return resp.json( newUser )
 
-        const newUser = await prisma.user.create({
-            data: {
-                name,
-                email,
-                password
-            }
-        })
-        
-        return resp.json( newUser )
-
-    } catch (error) {
-        console.log('línea15:', error);
-      if (error) next(error);
-    }
+  } catch (error) {
+    console.log(error)
+    if (error) next(error);
+  }
 };
 
 module.exports = {
