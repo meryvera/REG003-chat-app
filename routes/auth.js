4e48d31f1152ba71729935116a07064b34c10078
Yessenia Vela <vela.kathy@gmail.com>
2021-10-18 12:25:20 -0500

    create route auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,2 +1,9 @@
-//TODO: JWT
-//TODO: Decrypt
+const { signIn } = require('../controller/auth');
+
+/** @module auth */
+module.exports = (app, nextMain) => {
+
+  app.post('/auth', signIn);
+
+  return nextMain();
+};
