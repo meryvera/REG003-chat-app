c75a151e8bbaf99719957cff343b5d940c41c73e
César Michaell <carlasanchezcorrea>
2021-10-26 13:08:55 -0500

    splice disconnect userConnected /disconnected

--- index.js
+++ index.js
@@ -35,17 +35,35 @@ io.use(socketioJwt.authorize({
 }));
   
 /* socket */
-let connectedUsers=[];
+const connectedUsers=[];
 
 io.on('connection', (client) => { //on escucha eventos connection, 1 vez que hay respuesta del cb (socket), manejo esas asincronioas
   console.log('Nuevo usuario conectado',  client.id);
-      connectedUsers.push({
-      userID: client.decoded_token.id ,
-      username: client.decoded_token.name 
-    })
+  connectedUsers.push({
+    userID: client.decoded_token.id ,
+    username: client.decoded_token.name ,
+    idSocket: client.id,
+    email:client.decoded_token.email ,
+  })
   console.log('ES EL ARRAY', connectedUsers);
 
+  const usersConnectedId = connectedUsers.map(( e )=> e.userID);
+
+
+  const uniqueUsers = new Set(usersConnectedId);
+
+  const uniqueUsersArray = [...uniqueUsers]
+
+
+ // console.log('Id únicos', uniqueUsersArray);
+
+
+
+
   io.emit("connectedUsers", connectedUsers);
+
+
+
   //++currentUsers
   // the server gets it as a chat message event
   client.on('sendMessage', (messageInfo) => {
@@ -54,7 +72,16 @@ io.on('connection', (client) => { //on escucha eventos connection, 1 vez que hay
     client.broadcast.emit('receiveMessage', messageInfo); // mandado del BE hacia el FE
   });
 
-  socket.disconnect(client, connectedUsers);
+  //socket.disconnect(client, connectedUsers);
+
+  client.on("disconnect", function() {
+    connectedUsers.splice(connectedUsers.indexOf(client.userID), 1);
+    // updateUsernames();
+    // connections.splice(connections.indexOf(client), 1);
+    console.log("disconnected socket", connectedUsers)
+    io.emit("connectedUsers", connectedUsers);
+
+  });
 });
 
 // Registrar rutas
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,27 @@
 const socketIO = require('socket.io');
 
 const disconnect = ( client, connectedUsers ) => {
+
     
-  client.on('disconnect', () => {
-    console.log('Usuario desconectado');
-  })
+  client.on("disconnect", function() {
+    connectedUsers.splice(connectedUsers.indexOf(client.userID), 1);
+    // updateUsernames();
+    // connections.splice(connections.indexOf(client), 1);
+    console.log("disconnected socket", connectedUsers)
+    //io.emit("connectedUsers", connectedUsers);
+
+});
+
   
   //connectedUsers.splice(connectedUsers.indexOf(client), 1);
 
 }
+
+
+// function updateUsernames() {
+//   io.emit("get users", connectedUsers);
+// };
+
 module.exports = {
   disconnect
 }
