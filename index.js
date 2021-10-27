a523e3fe5c6699ecf01826069af1839a4eb1b3d9
César Michaell <carlasanchezcorrea>
2021-10-27 09:24:13 -0500

    eliminar objetos duplicados

--- index.js
+++ index.js
@@ -38,42 +38,58 @@ io.use(socketioJwt.authorize({
 const connectedUsers=[];
 
 io.on('connection', (client) => { //on escucha eventos connection, 1 vez que hay respuesta del cb (socket), manejo esas asincronioas
-  console.log('Nuevo usuario conectado',  client.id);
+  // console.log('Nuevo usuario conectado',  client.id);
   connectedUsers.push({
     userID: client.decoded_token.id ,
     username: client.decoded_token.name ,
     idSocket: client.id,
     email:client.decoded_token.email ,
   })
-  console.log('ES EL ARRAY', connectedUsers);
-
-  const usersConnectedId = connectedUsers.map(( e )=> e.userID);
 
 
-  const uniqueUsers = new Set(usersConnectedId);
+ // console.log('ES EL ARRAY', connectedUsers);
 
-  const uniqueUsersArray = [...uniqueUsers]
-
-
- // console.log('Id únicos', uniqueUsersArray);
+  //const usersConnectedId = connectedUsers.map(( e )=> e.userID);
 
 
+  // const uniqueUsers = new Set(usersConnectedId);
+
+  // const uniqueUsersArray = [...uniqueUsers]
 
 
-  io.emit("connectedUsers", connectedUsers);
+  // console.log('Id únicos', uniqueUsersArray);
 
+  const userName = client.decoded_token.name;
 
+  const burger = client.decoded_token
 
-  //++currentUsers
+  const setCart = [];
+
+ // console.log(filteredCategories);
+  const arrayVacio = [];
+  connectedUsers.forEach(category => {
+      if (!arrayVacio.find(cat => cat.userID == category.userID )) {
+          const { userID, username, idSocket, email } = category;
+          arrayVacio.push({ userID, username, idSocket, email});
+      }
+  });
+
+ // console.log('arrayVacio', arrayVacio);
+  const usersConnectedId = arrayVacio.map(( e )=> e.username);
+ // console.log('usersConnectedId', usersConnectedId);
+
+  client.emit("userName", userName);
+
+  io.emit("connectedUsers", usersConnectedId);
+
   // the server gets it as a chat message event
   client.on('sendMessage', (messageInfo) => {
-    console.log('desde elk array',connectedUsers)
-    console.log('message: ' + messageInfo.text); //message: Hola Kathy Angular - recibido desde el FE
+    console.log('message: ' , messageInfo.text , client.decoded_token.name); //message: Hola Kathy Angular - recibido desde el FE
     client.broadcast.emit('receiveMessage', messageInfo); // mandado del BE hacia el FE
   });
 
   //socket.disconnect(client, connectedUsers);
-
+  
   client.on("disconnect", function() {
     connectedUsers.splice(connectedUsers.indexOf(client.userID), 1);
     // updateUsernames();
